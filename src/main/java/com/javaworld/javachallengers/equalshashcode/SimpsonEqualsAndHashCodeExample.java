f56a7f73c6240f9ff01ceef779c1a9431216a223
rafael <rafacdelnero@gmail.com>
2018-09-04 23:05:59 +0100

    Changed weight of Bart.

--- src/main/java/com/javaworld/javachallengers/equalshashcode/SimpsonEqualsAndHashCodeExample.java
+++ src/main/java/com/javaworld/javachallengers/equalshashcode/SimpsonEqualsAndHashCodeExample.java
@@ -4,7 +4,7 @@ public class SimpsonEqualsAndHashCodeExample {
 
     public static void main(String... equalsExplanation) {
         System.out.println(new Simpson("Homer", 35, 120).equals(new Simpson("Homer", 35, 120)));
-        System.out.println(new Simpson("Bart", 10, 120).equals(new Simpson("El Barto", 10, 45)));
+        System.out.println(new Simpson("Bart", 10, 45).equals(new Simpson("El Barto", 10, 45)));
         System.out.println(new Simpson("Lisa", 54, 60).equals(new Object()));
     }
 
