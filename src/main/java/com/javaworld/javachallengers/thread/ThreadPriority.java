3aed4bb661dc6928762fea4a5d633dd40d53a966
rafadelnero <rafacdelnero@gmail.com>
2018-12-11 20:34:55 +0000

    Formatted the code.

--- src/main/java/com/javaworld/javachallengers/thread/ThreadPriority.java
+++ src/main/java/com/javaworld/javachallengers/thread/ThreadPriority.java
@@ -3,13 +3,13 @@ package com.javaworld.javachallengers.thread;
 public class ThreadPriority {
 
     public static void main(String... threadPriority) {
-        Thread homerThread = new Thread(() -> System.out.println("Homer"));
         Thread moeThread = new Thread(() -> System.out.println("Moe"));
         Thread barneyThread = new Thread(() -> System.out.println("Barney"));
+        Thread homerThread = new Thread(() -> System.out.println("Homer"));
 
-        homerThread.setPriority(Thread.MIN_PRIORITY);
-        barneyThread.setPriority(Thread.NORM_PRIORITY);
         moeThread.setPriority(Thread.MAX_PRIORITY);
+        barneyThread.setPriority(Thread.NORM_PRIORITY);
+        homerThread.setPriority(Thread.MIN_PRIORITY);
 
         homerThread.start();
         barneyThread.start();
